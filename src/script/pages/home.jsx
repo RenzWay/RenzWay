d98d634bf9219e4d021a67da0e7484e623df693b
RenzWay <dikakamza@gmail.com>
2025-06-30 14:51:01 +0700

    update again

--- src/script/pages/home.jsx
+++ src/script/pages/home.jsx
@@ -60,7 +60,7 @@ export default function HomePage() {
           <img
             src="https://i.pravatar.cc/40?img=3"
             alt="Logo"
-            className="w-10 h-10 md:w-12 md:h-12 rounded-full border-2 border-indigo-400 object-cover sm:hidden"
+            className="w-10 h-10 md:w-12 md:h-12 rounded-full border-2 border-indigo-400 object-cover"
           />
           <span className="text-2xl md:text-3xl font-bold text-gray-900">
             RenzWay
@@ -143,7 +143,7 @@ export default function HomePage() {
 
       {/* Hero & Stats */}
       <div className="flex flex-col md:flex-row items-center justify-center flex-1 px-6 md:px-12 py-16 md:py-24 gap-12 md:gap-20">
-        <div className="flex-1 flex flex-col items-start text-center md:text-left">
+        <div className="flex-1 flex flex-col items-center md:items-start text-center md:text-left">
           <h1 className="text-4xl md:text-6xl font-extrabold text-gray-900 leading-tight mb-4">
             Hi, I'm <span className="text-indigo-600">RenzWay</span> 👋
           </h1>
@@ -154,7 +154,7 @@ export default function HomePage() {
             <span ref={el} id="introdution"></span>
           </p>
 
-          <div className="flex flex-1/2 gap-6 mb-6">
+          <div className="flex gap-6 mb-6 sm:justify-center sm:items-center">
             {/* Ikon Sosial Media dari React Icons */}
             <a
               href="https://github.com/RenzWay"
@@ -181,14 +181,18 @@ export default function HomePage() {
               <FaInstagram size={32} /> {/* Menggunakan ikon Instagram */}
             </a>
           </div>
-          <button className="bg-indigo-600 text-white font-semibold py-3 px-8 rounded-full shadow-lg hover:shadow-xl hover:bg-indigo-700 transition-all duration-300 transform hover:-translate-y-1">
-            Download CV
-          </button>
+
+          <div className="flex sm:justify-center">
+            <button className="bg-indigo-600 text-white font-semibold py-3 px-8 rounded-full shadow-lg hover:shadow-xl hover:bg-indigo-700 transition-all duration-300 transform hover:-translate-y-1">
+              Download CV
+            </button>
+          </div>
         </div>
         <img
           src="https://i.pravatar.cc/300?img=3" // Gambar profil yang lebih besar
           alt="Profile"
-          className="w-72 h-72 sm:hidden md:hidden md:w-96 md:h-96 rounded-full object-cover shadow-2xl border-4 border-indigo-400 transform hover:scale-105 transition-transform duration-300"
+          className="w-72 h-72 md:w-96 md:h-96 rounded-full object-cover shadow-2xl border-4 border-indigo-400 transform hover:scale-105 transition-transform duration-300"
+          id="profile"
         />
       </div>
 
